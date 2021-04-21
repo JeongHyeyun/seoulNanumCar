c9bba294e81708c3da3ee53ac81390861826d724
Hyeyun Jeong <yuuuuuuni@gmail.com>
2021-04-21 21:07:59 +0900

    kakao map update

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="map">
-    <div id="test" style="widht: 500px; height: 500px;"></div>
+  <div id="map-wrap">
+    <div id="map" style="widht: 500px; height: 500px;"></div>
   </div>
 </template>
 
 <script>
 export default {
   mounted() {
-    if (window.kakao && window.kakao.maps) {
-      const container = document.getElementById('test');
-      const options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
+    if (window.kakao && window.kakao.maps) { // kakao map이 있을 때,
+      const container = document.getElementById('map'); // 지도 그릴 곳
+      const options = { // 기본 옵션
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 위도,경도
+        level: 3, // 축소
       };
-      const map = new kakao.maps.Map(container, options);
+      const map = new kakao.maps.Map(container, options); // 지도 생성
       map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
-    } else {
+    } else { // kakao map이 없을 때,
       const script = document.createElement('script');
       /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
@@ -24,12 +24,6 @@ export default {
     }
   },
   methods: {
-    init() {
-      console.log('init');
-    },
-    add() {
-      console.log('add');
-    },
   },
   created() {
   },
